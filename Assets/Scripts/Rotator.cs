ca9550bcac059f350a8cb8f57fa2fbfed1d5dd3f
Goblinus <storedenen@gmail.com>
2022-09-11 18:31:38 +0200

    added project

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    public float rotationSpeed = 1f;
+
+    [SerializeField]
+    public Vector3 rotationDirection = Vector3.left;
+
+    void Update()
+    {
+        Vector3 rotateAroundVector = Quaternion.AngleAxis(90, Vector3.forward) * rotationDirection;
+
+        this.transform.Rotate(rotateAroundVector * Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 lineEnd = this.transform.position + rotationDirection * rotationSpeed;
+
+        Gizmos.DrawLine(this.transform.position, lineEnd);
+    }
+}
