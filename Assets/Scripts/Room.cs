a12aad848302c4146ffd8e7f45015181d6447bb8
Goblinus <storedenen@gmail.com>
2022-09-14 11:35:02 +0200

    added room script

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DoorPosition
+{
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3
+}
+
+public class Room : MonoBehaviour
+{
+}
